a81618011dce224f383f016c7a24a99f5ff5571e
Fatih Kocyigit <kocyigit.mf@hotmail.com>
2023-11-28 09:17:45 +0100

    hai

--- apimaatjes/apimaatjes/Form1.Designer.cs
+++ apimaatjes/apimaatjes/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.strAandeel = new System.Windows.Forms.Button();
+            this.txtDbInfo = new System.Windows.Forms.TextBox();
+            this.DBlbl = new System.Windows.Forms.Label();
+            this.txtApiInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dbFetchtxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(650, 211);
+            this.textBox1.TabIndex = 0;
+            // 
+            // strAandeel
+            // 
+            this.strAandeel.Location = new System.Drawing.Point(192, 320);
+            this.strAandeel.Name = "strAandeel";
+            this.strAandeel.Size = new System.Drawing.Size(75, 23);
+            this.strAandeel.TabIndex = 1;
+            this.strAandeel.Text = "aandelen";
+            this.strAandeel.UseVisualStyleBackColor = true;
+            this.strAandeel.Click += new System.EventHandler(this.strAandeel_Click);
+            // 
+            // txtDbInfo
+            // 
+            this.txtDbInfo.Location = new System.Drawing.Point(273, 297);
+            this.txtDbInfo.Multiline = true;
+            this.txtDbInfo.Name = "txtDbInfo";
+            this.txtDbInfo.ReadOnly = true;
+            this.txtDbInfo.Size = new System.Drawing.Size(188, 114);
+            this.txtDbInfo.TabIndex = 2;
+            this.txtDbInfo.TextChanged += new System.EventHandler(this.txtDbInfo_TextChanged);
+            // 
+            // DBlbl
+            // 
+            this.DBlbl.AutoSize = true;
+            this.DBlbl.Location = new System.Drawing.Point(328, 278);
+            this.DBlbl.Name = "DBlbl";
+            this.DBlbl.Size = new System.Drawing.Size(50, 16);
+            this.DBlbl.TabIndex = 3;
+            this.DBlbl.Text = "DB info";
+            // 
+            // txtApiInfo
+            // 
+            this.txtApiInfo.Location = new System.Drawing.Point(467, 297);
+            this.txtApiInfo.Multiline = true;
+            this.txtApiInfo.Name = "txtApiInfo";
+            this.txtApiInfo.ReadOnly = true;
+            this.txtApiInfo.Size = new System.Drawing.Size(188, 114);
+            this.txtApiInfo.TabIndex = 4;
+            this.txtApiInfo.TextChanged += new System.EventHandler(this.txtApiInfo_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "api info";
+            // 
+            // dbFetchtxt
+            // 
+            this.dbFetchtxt.Location = new System.Drawing.Point(12, 297);
+            this.dbFetchtxt.Multiline = true;
+            this.dbFetchtxt.Name = "dbFetchtxt";
+            this.dbFetchtxt.ReadOnly = true;
+            this.dbFetchtxt.Size = new System.Drawing.Size(188, 114);
+            this.dbFetchtxt.TabIndex = 6;
+            this.dbFetchtxt.TextChanged += new System.EventHandler(this.dbFetchtxt_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "DB fetch info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dbFetchtxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApiInfo);
+            this.Controls.Add(this.DBlbl);
+            this.Controls.Add(this.txtDbInfo);
+            this.Controls.Add(this.strAandeel);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button strAandeel;
+        private System.Windows.Forms.TextBox txtDbInfo;
+        private System.Windows.Forms.Label DBlbl;
+        private System.Windows.Forms.TextBox txtApiInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dbFetchtxt;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- apimaatjes/apimaatjes/Form1.cs
+++ apimaatjes/apimaatjes/Form1.cs
@@ -4,14 +4,166 @@ using System.Data.SqlClient;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MySql.Data.MySqlClient;
 
 namespace apimaatjes
 {
     public partial class Form1 : Form
     {
+        string connectionString = "Server=localhost;Database=apimaatjes;Uid=root;Pwd=;";
+        MySqlConnection connection;
+        private HttpClient httpClient;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Instantieer de HttpClient in de constructor van het formulier
+            httpClient = new HttpClient();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                MessageBox.Show("Database verbonden");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database niet verbonden: " + ex.Message);
+            }
+        }
+
+
+        private async Task MakeApiRequest()
+        {
+            try
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://apistocks.p.rapidapi.com/monthly?symbol=AAPL&dateStart=2021-01-01&dateEnd=2021-07-31"),
+                    Headers =
+            {
+                { "X-RapidAPI-Key", "4a7901e120msh2b0798676847123p1490aejsn76d155a96d73" },
+                { "X-RapidAPI-Host", "apistocks.p.rapidapi.com" },
+            },
+                };
+
+                using (var response = await httpClient.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    // Deserialiseer de JSON-respons naar een object
+                    var apiData = JsonConvert.DeserializeObject<ApiData>(json);
+
+                    // Sla de gegevens op in de database
+                    SaveToDatabase(apiData);
+
+                    MessageBox.Show("API-gegevens succesvol opgehaald en opgeslagen in de database.");
+                    txtApiInfo.Text = "API-gegevens succesvol opgehaald en opgeslagen in de database.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fout bij het ophalen van API-gegevens: " + ex.Message);
+                txtApiInfo.Text = ("Fout bij het ophalen van API-gegevens: " + ex.Message);
+            }
+        }
+
+
+        private void SaveToDatabase(ApiData apiData)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    int index = 1;
+                    foreach (var result in apiData.Results)
+                    {
+                        using (var command = new MySqlCommand("INSERT INTO stocks_info (symbol, company_name, open_price, low_price, volume, adj_close, date) VALUES (@Symbol, @CompanyName, @Open, @Low, @Volume, @AdjClose, @Date)", connection))
+                        {
+
+                            command.Parameters.AddWithValue("@Open", result.Open);
+                            command.Parameters.AddWithValue("@Close", result.Close);
+                            command.Parameters.AddWithValue("@High", result.High);
+                            command.Parameters.AddWithValue("@Low", result.Low);
+                            command.Parameters.AddWithValue("@Volume", result.Volume);
+                            command.Parameters.AddWithValue("@AdjClose", result.AdjClose);
+                            command.Parameters.AddWithValue("@Date", result.Date);
+
+                            command.Parameters.AddWithValue("@Symbol", apiData.Metadata.Symbol);
+                            command.Parameters.AddWithValue("@CompanyName", "YourCompanyName"); 
+
+                            command.ExecuteNonQuery();
+
+                            // Voeg de gegevens toe aan de TextBox in het gewenste formaat
+                            textBox1.AppendText($"{index}:\n");
+                            textBox1.AppendText($"Date: \"{result.Date}\"\n");
+                            textBox1.AppendText($"Open: {result.Open}\n");
+                            textBox1.AppendText($"Close: {result.Close}\n");
+                            textBox1.AppendText($"High: {result.High}\n");
+                            textBox1.AppendText($"Low: {result.Low}\n");
+                            textBox1.AppendText($"Volume: {result.Volume}\n");
+                            textBox1.AppendText($"AdjClose: {result.AdjClose}\n\n");
+
+                            index++;
+                            string lala = "succesvol opgehaald";
+                            dbFetchtxt.Text = lala; 
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("fout in de fetch is:" + ex.Message);
+                dbFetchtxt.Text = "fout in de fetch is:" + ex.Message;   
+            }
+        }
+
+        private async void strAandeel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await MakeApiRequest();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("fout button is: " + ex.Message);
+            }
+        }
+
+
+
+        public class ApiData
+        {
+            public Metadata Metadata { get; set; }
+            public Result[] Results { get; set; }
+        }
+
+        public class Metadata
+        {
+            public string Symbol { get; set; }
+            public string Interval { get; set; }
+            public string Timezone { get; set; }
+        }
+
+        public class Result
+        {
+            public string Date { get; set; }
+            public double Open { get; set; }
+            public double Close { get; set; }
+            public double High { get; set; }
+            public double Low { get; set; }
+            public long Volume { get; set; }
+            public double AdjClose { get; set; }
         }
     }
+
 }
